8922c3b8f109f15824f6b18ba274408c7519392e
Mike Zakaryan <mikezakaryan@Mikes-MacBook-Pro.local>
2024-11-18 00:43:48 +0400

    fix: anchor tag in the burger menu for navigation

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const burgurButton = document.getElementById("burgerButton");
+const navListContainer = document.getElementById("navListContainer");
 const nav = document.getElementById("nav");
 let isMenuOpen = false;
 
 burgurButton.addEventListener("click", () => {
-  console.log("working");
   if (!isMenuOpen) {
     isMenuOpen = true;
     nav.classList.add("nav-container-open");
   } else {
+    isMenuOpen = false;
+    nav.classList.remove("nav-container-open");
+  }
+});
+
+navListContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "a") {
     isMenuOpen = false;
     nav.classList.remove("nav-container-open");
   }
